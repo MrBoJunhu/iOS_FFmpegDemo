e59eed6b253f3d26cbb0ea1734fa07ddfa1fbc5f
bill <bjh89210@163.com>
2016-09-18 12:53:20 +0800

    工程初始化仓库

---
+++ iOS_FFmpegDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_FFmpegDemo
+//
+//  Created by bill on 16/9/18.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_FFmpegDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_FFmpegDemo
+//
+//  Created by bill on 16/9/18.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
